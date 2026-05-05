2868a90f8f31241ee64cab3e0c1005baf29caf9b
Mahmoud <mahmodwael111@gmail.com>
2026-05-06 02:04:16 +0300

    feat: add file upload with unique path generation - Fix indentation and name errors in data.py - Correct Pydantic settings parsing in .env - Fix module import errors in routes

--- src/controllers/ProjectController.py
+++ src/controllers/ProjectController.py
@@ -9,8 +9,8 @@ class ProjectController(BaseController):
     def __init__(self):
         super().__init__()
 
-    def get_project_details(self, project_id: str):
-        project_dir = os.path.join(self.file_dir, project_id)
+    def get_project_path(self, project_id: str):
+        project_dir = os.path.join(self.files_dir, project_id)
 
         if not os.path.exists(project_dir):
             os.makedirs(project_dir)
